72c4fb47b909d00771d9bbaf7135ce7f101a63cc
Marlody <marlodycairo@hotmail.com>
2021-05-21 14:55:17 -0500

    Add project files.

---
+++ AppPruebaRodandoProducts.Api.ApplicationService/ClienteApplication.cs
@@ -0,0 +1,15 @@
+﻿using AppPruebaRodandoProducts.Api.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPruebaRodandoProducts.Api.ApplicationService
+{
+    public class ClienteApplication : IClienteApplication
+    {
+        public string EnviarMensaje()
+        {
+            return "Testing...";
+        }
+    }
+}
